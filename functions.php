96944582f71feb8ec55f81f8f0485406a3fbd332
Igor Sakelari <sakelari.igor@gmail.com>
2018-08-29 22:58:57 -0400

    Preconnect for Goggle Fonts

--- functions.php
+++ functions.php
@@ -113,6 +113,27 @@ function odessaflex_fonts_url() {
 }
 
 /**
+ * Add preconnect for Google Fonts.
+ *
+ * @since OdessaFlex 1.0.0
+ *
+ * @param array  $urls           URLs to print for resource hints.
+ * @param string $relation_type  The relation type the URLs are printed.
+ * @return array $urls           URLs to print for resource hints.
+ */
+function odessaflex_resource_hints( $urls, $relation_type ) {
+	if ( wp_style_is( 'odessaflex-fonts', 'queue' ) && 'preconnect' === $relation_type ) {
+		$urls[] = array(
+			'href' => 'https://fonts.gstatic.com',
+			'crossorigin',
+		);
+	}
+
+	return $urls;
+}
+add_filter( 'wp_resource_hints', 'odessaflex_resource_hints', 10, 2 );
+
+/**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
  * Priority 0 to make it available to lower priority callbacks.
