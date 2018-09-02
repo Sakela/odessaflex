cd613908febf1d1958f0ebe7e7fd5c9d13cd3169
Igor Sakelari <sakelari.igor@gmail.com>
2018-09-02 15:42:02 -0400

    implement dropdown toggle functionality

--- js/navigation.js
+++ js/navigation.js
@@ -12,24 +12,16 @@
 
 		// Add dropdown toggle that displays child menu items.
 		var dropdownToggle = $( '<button />', { 'class': 'dropdown-toggle', 'aria-expanded': false })
-			.append( odessaflexScreenReaderText.icon )
+			.append( $( '<span />', { 'class': 'dropdown-symbol', text: '+' }) )
 			.append( $( '<span />', { 'class': 'screen-reader-text', text: odessaflexScreenReaderText.expand }) );
 
 		container.find( '.menu-item-has-children > a, .page_item_has_children > a' ).after( dropdownToggle );
 
-		// Set the active submenu dropdown toggle button initial state.
-		container.find( '.current-menu-ancestor > button' )
-			.addClass( 'toggled-on' )
-			.attr( 'aria-expanded', 'true' )
-			.find( '.screen-reader-text' )
-			.text( odessaflexScreenReaderText.collapse );
-		// Set the active submenu initial state.
-		container.find( '.current-menu-ancestor > .sub-menu' ).addClass( 'toggled-on' );
-
 		container.find( '.dropdown-toggle' ).click( function( e ) {
 			var _this = $( this ),
-				screenReaderSpan = _this.find( '.screen-reader-text' );
-
+			screenReaderSpan = _this.find( '.screen-reader-text' );
+			toggleSymbol = _this.find( '.dropdown-symbol' );
+			
 			e.preventDefault();
 			_this.toggleClass( 'toggled-on' );
 			_this.next( '.children, .sub-menu' ).toggleClass( 'toggled-on' );
@@ -37,6 +29,7 @@
 			_this.attr( 'aria-expanded', _this.attr( 'aria-expanded' ) === 'false' ? 'true' : 'false' );
 
 			screenReaderSpan.text( screenReaderSpan.text() === odessaflexScreenReaderText.expand ? odessaflexScreenReaderText.collapse : odessaflexScreenReaderText.expand );
+			toggleSymbol.text( toggleSymbol.text() === '-' ? '+' : '-' );
 		});
 	}
 
