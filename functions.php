d3d75bf3fdf3fc0066c9b955ac720b1ac879c6a1
Igor Sakelari <sakelari.igor@gmail.com>
2018-09-02 11:29:59 -0400

    Enqueue jquery to mimic twentyseventeen dropdown functionality

--- functions.php
+++ functions.php
@@ -175,7 +175,11 @@ function odessaflex_scripts() {
 
 	wp_enqueue_style( 'odessaflex-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'odessaflex-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'odessaflex-navigation', get_template_directory_uri() . '/js/navigation.js', array('jquery'), '20151215', true );
+	wp_localize_script( 'odessaflex-navigation', 'odessaflexScreenReaderText', array(
+		'expand' => __( 'Expand child menu', 'odessaflex'),
+		'collapse' => __( 'Collapse child menu', 'odessaflex'),
+	));
 
 	wp_enqueue_script( 'odessaflex-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
